d632f0ddf6bc67b95a35ae9824ae35da94353b37
daniellee <danieljoonleee@gmail.com>
2022-12-11 12:37:33 -0500

    completed?

--- src/Game.java
+++ src/Game.java
@@ -5,8 +5,7 @@ public class Game {
     }
 
     public void startGame(){
-        System.out.println("Welcome to hangman. " +
-                "Guess the word before you run out of limbs!");
+        System.out.println("Welcome to hangman. Guess the word before you run out of limbs!");
         boolean repeat = true;
         while (repeat){
             System.out.print("Would you like to pick your topic? y/n: ");
@@ -17,7 +16,7 @@ public class Game {
                 game1 = new Hangman();
                 ansAsNum = 1;
             } else {
-                System.out.print("Pick from the following topics " + "(school subject, color, holiday, animal, fruit): ");
+                System.out.print("Pick from the following topics (school subject, color, holiday, animal, fruit): ");
                 String topic = scan.nextLine();
                 game1 = new Hangman(topic);
                 ansAsNum = 2;
